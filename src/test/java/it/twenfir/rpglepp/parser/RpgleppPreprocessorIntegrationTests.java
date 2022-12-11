2fc6673a34742cf91604e653cbe150cbe843ec7a
Nicola Musatti <nicola.musatti@gmail.com>
2022-12-11 10:27:51 +0100

    Correct tests' package

--- src/test/java/it/twenfir/rpglepp/parser/RpgleppPreprocessorIntegrationTests.java
+++ src/test/java/it/twenfir/rpglepp/parser/RpgleppPreprocessorIntegrationTests.java
@@ -1,4 +1,4 @@
-package io.github.twentyfirst.rpglepp.parser;
+package it.twenfir.rpglepp.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
