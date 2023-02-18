0239fca5f2dbedb8f4702f5e9347ebd1f7ba322d
Nicola Musatti <nicola.musatti@gmail.com>
2023-02-18 15:58:35 +0100

    Allow underscores in filenames

--- src/main/java/it/twenfir/rpglepp/parser/DefaultCopyBookReader.java
+++ src/main/java/it/twenfir/rpglepp/parser/DefaultCopyBookReader.java
@@ -15,7 +15,7 @@ public class DefaultCopyBookReader implements CopyBookReader {
 
 	private static String[] COPY_EXTENSIONS = new String[] { ".RPGLECOPY", ".RPGLE", ".SQLRPGLE" };
 
-    private static Pattern NAME_PATTERN = Pattern.compile("/?(?:[A-Za-z*]+(?:[,/]))*((?:\\w+)(?:\\.\\w+)?)");
+    private static Pattern NAME_PATTERN = Pattern.compile("/?(?:[A-Za-z*_]+(?:[,/]))*((?:\\w+)(?:\\.\\w+)?)");
 
     private List<String> copyBookPath;
 
